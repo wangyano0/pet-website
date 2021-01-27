1d43491f00d34f956ac9735d8e26789ce522dd07
dujianing-n <75530330+dujianing-n@users.noreply.github.com>
2021-01-27 22:24:50 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
-Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App) 
+});
